3231bd82e8483c22797cceda2cdca6e13dae6177
Frederick Johnson <fritzj96@gmail.com>
2021-05-19 11:20:32 -0500

    added: animation fix + menu content tweak

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,6 +10,12 @@ function openMenu() {
 function closeMenu() {
   const menu = document.getElementById("mobile-menu");
   const menuBackdrop = document.getElementById("mobile-menu-backdrop");
-  menu.classList.add("hidden");
-  menuBackdrop.classList.add("hidden");
+  menu.classList.add("fade-out");
+  menuBackdrop.classList.add("fade-out");
+  setTimeout(() => {
+    menu.classList.add("hidden");
+    menuBackdrop.classList.add("hidden");
+    menu.classList.remove("fade-out");
+    menuBackdrop.classList.remove("fade-out");
+  }, 500)
 }
